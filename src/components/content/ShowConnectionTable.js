f8da281c5948e932c72a214c9671a918303c715b
U-LKAB\MLBJOS <MLBJOS@LKLI05714.corp.lkab.com>
2021-03-24 18:14:01 +0100

    Moving table to a component

---
+++ src/components/content/ShowConnectionTable.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import DateFormatter from '../content/DateFormatter';
+import '../page/ShowConnection.css';
+import '../page/Table.css';
+
+const ShowConnectionTable = (props) => {
+    // const props.data = props.data;
+
+    function checkEmpty(input) {
+        if (input) {
+            return input;
+        }
+        return "n/a";
+    }
+
+    if (props) {
+        return (
+            <main className="mainPage">
+                <div className="connectionNavBar">
+                    <Link
+                        to={`/update/head/${encodeURIComponent(props.data.head.Id)}`}
+                    >Redigera huvud</Link>
+                    <Link
+                        to={`/update/line/${encodeURIComponent(props.data.head.Id)}`}
+                    >Redigera Linje</Link>
+                    <Link
+                        to={`/delete/connection/${encodeURIComponent(props.data.head.Id)}`}
+                    >Radera Kopplingskort</Link>
+                </div>
+                <div className="headerData">
+                    <div className="headerBlock">
+                        <h5>Nummer:</h5>
+                        <p>
+                            {props.data.head.Number}
+                        </p>
+                    </div>
+                    <div className="headerBlock">
+                        <h5>Namn:</h5>
+                        <p>{checkEmpty(props.data.head.Name)}</p>
+                    </div>
+                    <div className="headerBlock">
+                        <h5>Funktion:</h5>
+                        <p>{checkEmpty(props.data.head.Func)}</p>
+                    </div>
+                    <div className="headerBlock">
+                        <h5>Address:</h5>
+                        <p>{checkEmpty(props.data.head.Address)}</p>
+                    </div>
+                    <div className="headerBlock">
+                        <h5>Skapad:</h5>
+                        <p>
+                            <DateFormatter input={props.data.head.Created} type={"created"} />
+                        </p>
+                    </div>
+                    <div className="headerBlock">
+                        <h5>Uppdaterad:</h5>
+                        <p>
+                            {props.data.head.Updated ?
+                                <DateFormatter
+                                    input={props.data.head.Updated}
+                                    type={"updated"}
+                                />
+                                : "n/a"
+                            }
+                        </p>
+                    </div>
+                    <div className="headerBlock">
+                        <h5>Apptyp:</h5>
+                        <p>{checkEmpty(props.data.head.Apptype)}</p>
+                    </div>
+                    <div className="headerBlock">
+                        <h5>Apptyp2:</h5>
+                        <p>{checkEmpty(props.data.head.ApptypeTwo)}</p>
+                    </div>
+                    <div className="headerBlock">
+                        <h5>Användarnamn:</h5>
+                        <p>{checkEmpty(props.data.head.UserId)}</p>
+                    </div>
+                    <div className="headerBlock">
+                        <h5>Användare:</h5>
+                        <p>{checkEmpty(props.data.head.UserFullName)}</p>
+                    </div>
+                    <div className="headerBlock">
+                        <h5>Ritning:</h5>
+                        <p>{checkEmpty(props.data.head.Drawing)}</p>
+                    </div>
+                    <div className="headerBlock">
+                        <h5>Dokument:</h5>
+                        <p>{checkEmpty(props.data.head.Document)}</p>
+                    </div>
+                    <div className="headerBlock">
+                        <h5>Dokument:</h5>
+                        <p>{checkEmpty(props.data.head.Comment)}</p>
+                    </div>
+                </div>
+                <table className="table table-scroll table-stacked">
+                    <thead>
+                        <tr>
+                            <th>Rad</th>
+                            <th>Notering</th>
+                            <th>Ställ</th>
+                            <th>Från fält</th>
+                            <th>Från nummer</th>
+                            <th>Från uttag</th>
+                            <th>          </th>
+                            <th>Till fält</th>
+                            <th>Till nummer</th>
+                            <th>Till uttag</th>
+                            <th>Kommentar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {props.data.line ? props.data.line.map(element => (
+                            <tr key={element.Id}>
+                                <td>{element.Position}</td>
+                                <td>{element.Note}</td>
+                                <td>{element.Rack}</td>
+                                <td>{element.FieldFrom}</td>
+                                <td>{element.NrFrom}</td>
+                                <td>{element.KlFrom}</td>
+                                <td className="arrow">--&gt;</td>
+                                <td>{element.FieldTo}</td>
+                                <td>{element.NrTo}</td>
+                                <td>{element.KlTo}</td>
+                                <td>{element.Comment}</td>
+                            </tr>
+                        )) : null}
+                    </tbody>
+                </table>
+            </main>
+        );
+    }
+};
+
+export default ShowConnectionTable;
--- src/components/page/ShowConnection.js
+++ src/components/page/ShowConnection.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import './ShowConnection.css';
-import DateFormatter from '../content/DateFormatter';
 import config from '../../config';
 import PropTypes from 'prop-types';
+import ShowConnectionTable from '../content/ShowConnectionTable';
+import './ShowConnection.css';
 
 class ShowConnection extends Component {
     constructor(props) {
@@ -12,7 +11,6 @@ class ShowConnection extends Component {
         this.url = `${config.baseUrl}connections?id=${this.id}&api_key=${config.apiKey}`;
         this.getContent = this.getContent.bind(this);
         this.changeHandler = this.changeHandler.bind(this);
-        // this.submitHandler = this.submitHandler.bind(this);
 
         this.state = {
             data: {
@@ -39,7 +37,6 @@ class ShowConnection extends Component {
             .then((response) => response.json())
             .then((res) => {
                 if (res.data) {
-                    // console.log(res.data);
                     this.setState({
                         data: res.data,
                         isloaded: true
@@ -66,79 +63,10 @@ class ShowConnection extends Component {
         if (!isloaded) {
             return <div className="loading"><h2>Loading data...</h2></div>;
         } else if (data.head.length === 0) {
-            console.log(data);
             return <div className="notFound"><h1>404 Page not found</h1></div>;
         } else {
-            console.log(data);
             return (
-                <main className="mainPage">
-                    <div className="connectionNavBar">
-                        <Link
-                            to={`/update/head/${encodeURIComponent(data.head.Id)}`}
-                        >Redigera huvud</Link>
-                        <Link
-                            to={`/update/line/${encodeURIComponent(data.head.Id)}`}
-                        >Redigera Linje</Link>
-                        <Link
-                            to={`/delete/connection/${encodeURIComponent(data.head.Id)}`}
-                        >Radera Kopplingskort</Link>
-                    </div>
-                    <div className="headerData">
-                        <p><b>Nummer:</b> {data.head.Number}</p>
-                        <p><b>Namn:</b> {data.head.Name}</p>
-                        <p><b>Funktion:</b> {data.head.Func}</p>
-                        <p><b>Address:</b> {data.head.Address}</p>
-                        <p>
-                            <b>Skapad: </b>
-                            <DateFormatter input={data.head.Created} type={"created"} />
-                        </p>
-                        <p>
-                            <b>Uppdaterad: </b>
-                            <DateFormatter input={data.head.Updated} type={"updated"} />
-                        </p>
-                        <p><b>Apptyp: </b>{data.head.Apptype}</p>
-                        <p><b>Apptyp2: </b>{data.head.ApptypeTwo}</p>
-                        <p><b>Användarnamn: </b>{data.head.UserId}</p>
-                        <p><b>Användare: </b>{data.head.UserFullName}</p>
-                        <p><b>Ritning: </b>{data.head.Drawing}</p>
-                        <p><b>Dokument: </b>{data.head.Document}</p>
-                        <p><b>Dokument: </b>{data.head.Comment}</p>
-                    </div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Position</th>
-                                <th>Notering</th>
-                                <th>Ställ</th>
-                                <th>Från fält</th>
-                                <th>Från nummer</th>
-                                <th>Från uttag</th>
-                                <th>          </th>
-                                <th>Till fält</th>
-                                <th>Till nummer</th>
-                                <th>Till uttag</th>
-                                <th>Kommentar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.line ? data.line.map(element => (
-                                <tr key={element.Id}>
-                                    <td>{element.Position}</td>
-                                    <td>{element.Note}</td>
-                                    <td>{element.Rack}</td>
-                                    <td>{element.FieldFrom}</td>
-                                    <td>{element.NrFrom}</td>
-                                    <td>{element.KlFrom}</td>
-                                    <td>----&gt;</td>
-                                    <td>{element.FieldTo}</td>
-                                    <td>{element.NrTo}</td>
-                                    <td>{element.KlTo}</td>
-                                    <td>{element.Comment}</td>
-                                </tr>
-                            )) : null}
-                        </tbody>
-                    </table>
-                </main>
+                <ShowConnectionTable data={data}/>
             );
         }
     }
